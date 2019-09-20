23b5e4b67e34682387a5a01cd8c886e9cc5c7eeb
Marcos Vieira <marcosvieira.ftw@gmail.com>
2019-09-20 15:21:14 -0300

    [add] Filter component.
    * Implemented the filter component and its behavior.
    * This component will be responsible to managing the filters of table.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,19 +1,57 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {PropTypes} from 'prop-types';
 import Checkbox from '@material-ui/core/Checkbox';
 
+/**
+ * The component responsible to managing the filters tools.
+ */
 class Filter extends Component {
+    constructor() {
+        super();
+        this.state = {
+            currentlyChecked: 'name'
+        }
+    }
 
-    render() {
+    /**
+     * Responsible to manage the state of checkbox and call the ordenation function according to user choice.
+     */
+    onChange(field) {
+        this.setState({currentlyChecked: field});
+        if (field === 'age') {
+            return this.props.dateOrderChanged()
+        }
+
+        return this.props.nameOrderChanged()
+    }
+
+    render () {
         return (
-        <div className="checkboxes">
-            <Checkbox/>
-            <label>Name</label>
-            <Checkbox/>
-            <label>Age</label>
-        </div>
+            <div className="checkboxes">
+                <Checkbox
+                    value="name"
+                    checked={this.state.currentlyChecked === 'name'}
+                    onChange={(e) => {this.onChange('name')} } />
+                <label>Name</label>
+                <Checkbox
+                    value="age"
+                    checked={this.state.currentlyChecked === 'age'}
+                    onChange={(e) => {this.onChange('age')} } />
+                <label>Age</label>
+            </div>
         );
     }
 }
+
+Filter.propTypes = {
+    /**
+     * Gets called when the user clicks on the Name checkbox.
+     */
+    nameOrderChanged: PropTypes.func.isRequired,
+    /**
+     * Gets called when the user clicks on the Age checkbox.
+     */
+    dateOrderChanged: PropTypes.func.isRequired
+};
 
 export default Filter;
